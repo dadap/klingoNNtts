d8e0f24e9eedb6b5598c501849a5166a97d34dad
Daniel Dadap <daniel@dadap.net>
2018-08-13 08:31:03 -0500

    Allow setup without Anaconda
    The switch to Anaconda was just to pick up support for Theano GPU
    acceleration. Since not everybody is going to be using that, in
    particular for setups that will be doing synthesis and not training.

--- setup.sh
+++ setup.sh
@@ -11,18 +11,38 @@ fi
 if [ ! -f $CONDA_HOME/etc/profile.d/conda.sh ]; then
     echo "Cannot find Anaconda: if it is already installed, set CONDA_HOME"
     echo "to the root of your Anaconda installation."
-    exit 1
+
+    echo "Type 'luq' and press enter if you want to proceed without Anaconda:"
+    echo "GPU support will not be available if you do this."
+
+    read resp
+
+    if [ "$resp" = "luq" ]; then
+        NOCONDA=1
+    else
+        exit 1
+    fi
+else
+    NOCONDA=
 fi
 
-. $CONDA_HOME/etc/profile.d/conda.sh
-
-if ! [ -x pyenv/bin/python ]; then
-    conda create -p ./pyenv python=2.7
+if [ "$NOCONDA" = "1" ]; then
+    virtualenv pyenv
+    . pyenv/bin/activate
+else
+    . $CONDA_HOME/etc/profile.d/conda.sh
+    if ! [ -x pyenv/bin/python ]; then
+        conda create -p ./pyenv python=2.7
+    fi
+    conda activate ./pyenv
 fi
-
-conda activate ./pyenv
 
 # Install numpy first to work around dependency resolution
 pip install numpy
 pip install -r requirements.txt
-conda install theano
+
+if [ "$NOCONDA" = "1" ]; then
+    pip install theano
+else
+    conda install theano
+fi
