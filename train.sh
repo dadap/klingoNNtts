0898ae7f7f8eee72d181420cbcdd58468e4926ea
Daniel Dadap <daniel@dadap.net>
2018-08-13 08:56:51 -0500

    Change default training parameters

--- train.sh
+++ train.sh
@@ -29,6 +29,14 @@ python ./scripts/train.py -s $TTSSPEAKER -l $TTSLANG $TTSMODEL
 export THEANO_FLAGS="device=cuda,floatX=float32"
 export MKL_THREADING_LAYER="GNU"
 
+sed -i -e "s/^training_epochs  : .*$/training_epochs  : 100/" train/$TTSLANG/speakers/$TTSSPEAKER/$TTSMODEL/processors/duration_predictor/config.cfg
+
+sed -i -e "s/^learning_rate    : .*$/learning_rate    : 0.0006/" train/$TTSLANG/speakers/$TTSSPEAKER/$TTSMODEL/processors/acoustic_predictor/config.cfg
+sed -i -e "s/^training_epochs  : .*$/training_epochs  : 350/" train/$TTSLANG/speakers/$TTSSPEAKER/$TTSMODEL/processors/acoustic_predictor/config.cfg
+sed -i -e "s/^warmup_epoch     : .*$/warmup_epoch     : 300/" train/$TTSLANG/speakers/$TTSSPEAKER/$TTSMODEL/processors/acoustic_predictor/config.cfg
+#sed -i -e "s/^hidden_layer_size  : .*$/hidden_layer_size  : [2048, 2048, 2048, 2048, 2048, 2048]/" train/$TTSLANG/speakers/$TTSSPEAKER/$TTSMODEL/processors/acoustic_predictor/config.cfg
+sed -i -e "s/^batch_size       : .*$/batch_size       : 2048/" train/$TTSLANG/speakers/$TTSSPEAKER/$TTSMODEL/processors/acoustic_predictor/config.cfg
+
 for predictor in duration acoustic; do
     python tools/merlin/src/run_merlin.py train/$TTSLANG/speakers/$TTSSPEAKER/$TTSMODEL/processors/${predictor}_predictor/config.cfg
     python scripts/util/store_merlin_model.py train/$TTSLANG/speakers/$TTSSPEAKER/$TTSMODEL/processors/${predictor}_predictor/config.cfg voices/$TTSLANG/$TTSSPEAKER/$TTSMODEL/processors/${predictor}_predictor
